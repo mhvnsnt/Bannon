ca07ef05fd995f467fcd5e2f34cb2113758b7ad8
mhvnsnt <fmmg.mars@gmail.com>
2026-07-14 22:03:22 -0400

    Add Taunt Manager CPP

--- unreal/Source/BannonEngine/Private/BannonTauntManager.cpp
+++ unreal/Source/BannonEngine/Private/BannonTauntManager.cpp
@@ -1,12 +1,7 @@
 // Copyright BANNON.
 #include "BannonTauntManager.h"
 
-FBannonBuffs UBannonTauntManager::ExecuteTaunt(FName TauntID)
+void UBannonTauntManager::ExecuteTaunt(AActor* Fighter, FName TauntID)
 {
-    FBannonBuffs Buffs;
-    if (TauntID == "Aggressive") { Buffs.Strength = 1.2f; Buffs.Momentum = 1.1f; }
-    else if (TauntID == "Speedster") { Buffs.Speed = 1.2f; Buffs.Momentum = 1.05f; }
-    
-    UE_LOG(LogTemp, Log, TEXT("Taunt executed: %s"), *TauntID.ToString());
-    return Buffs;
+    UE_LOG(LogTemp, Log, TEXT("Executing taunt: %s"), *TauntID.ToString());
 }
