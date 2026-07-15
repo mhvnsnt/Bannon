05cb270110fedc3e9816a1180d5be94368d55710
mhvnsnt <fmmg.mars@gmail.com>
2026-07-14 22:02:38 -0400

    Update Fighter Header for Visuals

--- unreal/Source/BannonEngine/Public/BannonFighterCharacter.h
+++ unreal/Source/BannonEngine/Public/BannonFighterCharacter.h
@@ -1,6 +1,7 @@
 // Copyright BANNON.
 #pragma once
 #include "CoreMinimal.h"
+#include "GameFramework/Character.h"
 #include "BannonFighterCharacter.generated.h"
 
 UCLASS()
@@ -8,10 +9,7 @@ class BANNONENGINE_API ABannonFighterCharacter : public ACharacter
 {
 	GENERATED_BODY()
 public:
-    // Visual Damage Rendering Props
-    UPROPERTY(BlueprintReadWrite, Category="Bannon|Visual") float HeadCutAlpha = 0.0f;
-    UPROPERTY(BlueprintReadWrite, Category="Bannon|Visual") float TorsoBruiseAlpha = 0.0f;
-    
-    // Impact logic stub
+    UPROPERTY(BlueprintReadWrite, Category="Bannon|Visual") float HeadCutAlpha;
+    UPROPERTY(BlueprintReadWrite, Category="Bannon|Visual") float TorsoBruiseAlpha;
     UFUNCTION(BlueprintCallable, Category="Bannon|Visual") void RefreshDamageMaterials();
 };
