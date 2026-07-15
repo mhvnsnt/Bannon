810a66036e335df835d0e45cac79c820561bdabf
mhvnsnt <fmmg.mars@gmail.com>
2026-07-14 22:03:21 -0400

    Implement Ground Game FSM

--- unreal/Source/BannonEngine/Private/BannonGroundGameManager.cpp
+++ unreal/Source/BannonEngine/Private/BannonGroundGameManager.cpp
@@ -3,5 +3,8 @@
 
 void UBannonGroundGameManager::TransitionPosition(AActor* Fighter, EGroundPosition NewPosition)
 {
-    UE_LOG(LogTemp, Log, TEXT("Transitioning ground position."));
+    // Implementation of FSM logic
+    UE_LOG(LogTemp, Log, TEXT("Transitioning ground position to %d"), (int32)NewPosition);
+    
+    // Wire up defensive struggle/offense technical skill matrix here
 }
