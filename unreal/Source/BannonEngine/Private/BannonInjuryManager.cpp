b4f48e24078f3e9e50d92b2135c61f466b04bacf
mhvnsnt <fmmg.mars@gmail.com>
2026-07-14 22:02:40 -0400

    Implement Injury Registration Hook

--- unreal/Source/BannonEngine/Private/BannonInjuryManager.cpp
+++ unreal/Source/BannonEngine/Private/BannonInjuryManager.cpp
@@ -1,7 +1,20 @@
 // Copyright BANNON.
 #include "BannonInjuryManager.h"
+#include "BannonFighterCharacter.h"
 
-void UBannonInjuryManager::RegisterInjury(AActor* Fighter, FName BodyPart, float Severity)
+void UBannonInjuryManager::RegisterInjury(AActor* Fighter, FName BodyPart, float DamageAmount)
 {
-    UE_LOG(LogTemp, Log, TEXT("Registering injury to %s with severity %f"), *BodyPart.ToString(), Severity);
+    ABannonFighterCharacter* FighterChar = Cast<ABannonFighterCharacter>(Fighter);
+    if (FighterChar)
+    {
+        if (BodyPart == FName("Head"))
+        {
+            FighterChar->HeadCutAlpha = FMath::Clamp(FighterChar->HeadCutAlpha + (DamageAmount * 0.01f), 0.0f, 1.0f);
+        }
+        else if (BodyPart == FName("Torso"))
+        {
+            FighterChar->TorsoBruiseAlpha = FMath::Clamp(FighterChar->TorsoBruiseAlpha + (DamageAmount * 0.01f), 0.0f, 1.0f);
+        }
+        FighterChar->RefreshDamageMaterials();
+    }
 }
