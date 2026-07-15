f877a9e09890113575fcbcfe36044a4cbd9b188a
mhvnsnt <fmmg.mars@gmail.com>
2026-07-14 22:03:32 -0400

    Bind Taunt Manager

--- unreal/Source/BannonEngine/Public/BannonFighterCharacter.h
+++ unreal/Source/BannonEngine/Public/BannonFighterCharacter.h
@@ -2,6 +2,7 @@
 #pragma once
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "BannonTauntManager.h"
 #include "BannonFighterCharacter.generated.h"
 
 UCLASS()
@@ -9,7 +10,12 @@ class BANNONENGINE_API ABannonFighterCharacter : public ACharacter
 {
 	GENERATED_BODY()
 public:
+    ABannonFighterCharacter();
+    UPROPERTY() UBannonTauntManager* TauntManager;
+    
     UPROPERTY(BlueprintReadWrite, Category="Bannon|Visual") float HeadCutAlpha;
     UPROPERTY(BlueprintReadWrite, Category="Bannon|Visual") float TorsoBruiseAlpha;
     UFUNCTION(BlueprintCallable, Category="Bannon|Visual") void RefreshDamageMaterials();
+    
+    UFUNCTION(BlueprintCallable, Category="Bannon|Taunt") void TriggerTaunt(FName TauntID);
 };
