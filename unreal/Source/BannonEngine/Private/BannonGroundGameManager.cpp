fb6473f966acf2deb1a703f71213e255f1758845
mhvnsnt <fmmg.mars@gmail.com>
2026-07-14 22:02:42 -0400

    Add Ground Game Manager CPP

---
+++ unreal/Source/BannonEngine/Private/BannonGroundGameManager.cpp
@@ -0,0 +1,7 @@
+// Copyright BANNON.
+#include "BannonGroundGameManager.h"
+
+void UBannonGroundGameManager::TransitionPosition(AActor* Fighter, EGroundPosition NewPosition)
+{
+    UE_LOG(LogTemp, Log, TEXT("Transitioning ground position."));
+}
