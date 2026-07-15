093aad406edde1307e2bd369dd1bb9b24dfc65af
mhvnsnt <fmmg.mars@gmail.com>
2026-07-14 21:55:14 -0400

    Implement Damage Rendering

--- unreal/Source/BannonEngine/Private/BannonFighterCharacter.cpp
+++ unreal/Source/BannonEngine/Private/BannonFighterCharacter.cpp
@@ -3,8 +3,5 @@
 
 void ABannonFighterCharacter::RefreshDamageMaterials()
 {
-    // Scalar Parameter Mapping
-    // GetMesh()->SetScalarParameterValueOnMaterials(FName("HeadCutAlpha"), HeadCutAlpha);
-    // GetMesh()->SetScalarParameterValueOnMaterials(FName("TorsoBruiseAlpha"), TorsoBruiseAlpha);
     UE_LOG(LogTemp, Log, TEXT("Refreshing damage materials: HeadCut=%f, TorsoBruise=%f"), HeadCutAlpha, TorsoBruiseAlpha);
 }
