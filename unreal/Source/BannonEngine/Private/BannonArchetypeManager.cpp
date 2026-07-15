f1caa5d620f2bbeddf3063d534315a137a9daabb
mhvnsnt <fmmg.mars@gmail.com>
2026-07-14 21:58:18 -0400

    Add Archetype Manager CPP

---
+++ unreal/Source/BannonEngine/Private/BannonArchetypeManager.cpp
@@ -0,0 +1,7 @@
+// Copyright BANNON.
+#include "BannonArchetypeManager.h"
+
+void UBannonArchetypeManager::ApplyArchetype(AActor* Fighter, EBannonArchetype Archetype)
+{
+    UE_LOG(LogTemp, Log, TEXT("Applying archetype to fighter"));
+}
