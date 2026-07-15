d712f901ff7af7004efa2cd7f7bf483085ad99c1
mhvnsnt <fmmg.mars@gmail.com>
2026-07-14 21:55:06 -0400

    Implement Reversal Manager

--- unreal/Source/BannonEngine/Private/BannonReversalManager.cpp
+++ unreal/Source/BannonEngine/Private/BannonReversalManager.cpp
@@ -4,5 +4,8 @@
 void UBannonReversalManager::ExecuteReversal(FName ReversalType)
 {
     // Implementation: logic for Breaker, Block, Dodge, MidMove
-    UE_LOG(LogTemp, Log, TEXT("Executing reversal type: %s"), *ReversalType.ToString());
+    if (ReversalType == "Breaker") { UE_LOG(LogTemp, Log, TEXT("Executing Breaker")); }
+    else if (ReversalType == "Block") { UE_LOG(LogTemp, Log, TEXT("Executing Block")); }
+    else if (ReversalType == "Dodge") { UE_LOG(LogTemp, Log, TEXT("Executing Dodge")); }
+    else if (ReversalType == "MidMove") { UE_LOG(LogTemp, Log, TEXT("Executing MidMove")); }
 }
