f050a0ae1bb0e9e2f1443411244d885c2d9fcb8f
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 13:24:29 -0400

    Implement Targeted Limb Striking System stub

---
+++ unreal/Source/BannonCore/Private/BannonTargetedLimbStrikingSystem.cpp
@@ -0,0 +1,11 @@
+#include "BannonTargetedLimbStrikingSystem.h"
+
+UBannonTargetedLimbStrikingSystem::UBannonTargetedLimbStrikingSystem()
+{
+    PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UBannonTargetedLimbStrikingSystem::BeginPlay()
+{
+    Super::BeginPlay();
+}
