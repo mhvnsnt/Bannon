8bde5d123d29147ced38ecdfd3096b45d8923530
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 13:24:24 -0400

    Implement Mid-Air Grapple Interceptions stub

---
+++ unreal/Source/BannonCore/Private/BannonMidAirGrappleInterceptions.cpp
@@ -0,0 +1,11 @@
+#include "BannonMidAirGrappleInterceptions.h"
+
+UBannonMidAirGrappleInterceptions::UBannonMidAirGrappleInterceptions()
+{
+    PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UBannonMidAirGrappleInterceptions::BeginPlay()
+{
+    Super::BeginPlay();
+}
