02c69a53144042e68f8bb9e1231f1422e0aa7159
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 13:23:47 -0400

    Implement Turnbuckle Deformation Physics stub

---
+++ unreal/Source/BannonCore/Private/BannonTurnbuckleDeformationPhysics.cpp
@@ -0,0 +1,11 @@
+#include "BannonTurnbuckleDeformationPhysics.h"
+
+UBannonTurnbuckleDeformationPhysics::UBannonTurnbuckleDeformationPhysics()
+{
+    PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UBannonTurnbuckleDeformationPhysics::BeginPlay()
+{
+    Super::BeginPlay();
+}
