e13bf5829fa67746bd8f246fcb0ee8f87b905d42
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 13:23:45 -0400

    Implement Rope Physics Simulation (Verlet) stub

---
+++ unreal/Source/BannonCore/Private/BannonRopePhysicsSimulationVerlet.cpp
@@ -0,0 +1,11 @@
+#include "BannonRopePhysicsSimulationVerlet.h"
+
+UBannonRopePhysicsSimulationVerlet::UBannonRopePhysicsSimulationVerlet()
+{
+    PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UBannonRopePhysicsSimulationVerlet::BeginPlay()
+{
+    Super::BeginPlay();
+}
