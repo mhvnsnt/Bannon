1270ff5f7f6e554dfd9601845dcc52ab4c2c9536
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 01:08:30 -0400

    Phase 40-42: Endgame Systems, Title Lineage, and APK Hotfix - unreal/Source/BannonCore/Public/BannonGodWithinEndgame.h

--- unreal/Source/BannonCore/Public/BannonGodWithinEndgame.h
+++ unreal/Source/BannonCore/Public/BannonGodWithinEndgame.h
@@ -10,6 +10,6 @@ class BANNONCORE_API UBannonGodWithinEndgame : public UObject
     GENERATED_BODY()
 
 public:
-    UFUNCTION(BlueprintCallable, Category="Bannon|Cosmic")
-    void ApplyRealityModifiers(float CosmicAlignment, UPARAM(ref) float& GravityScale, UPARAM(ref) float& GameSpeedMultiplier);
+    UFUNCTION(BlueprintCallable, Category="Bannon|Endgame")
+    void TriggerCosmicAlignment(float CosmicAlignmentScore, UPARAM(ref) bool& bUnlockRealityModifiers, UPARAM(ref) float& OutGravityScale, UPARAM(ref) float& OutTimeDilation);
 };
