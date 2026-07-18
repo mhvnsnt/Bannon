ae565744b054956ed433694e686c9aa5fa1d988a
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 13:24:12 -0400

    Implement Subway/Train Interactivity stub

---
+++ unreal/Source/BannonCore/Private/BannonSubwayTrainInteractivity.cpp
@@ -0,0 +1,11 @@
+#include "BannonSubwayTrainInteractivity.h"
+
+UBannonSubwayTrainInteractivity::UBannonSubwayTrainInteractivity()
+{
+    PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UBannonSubwayTrainInteractivity::BeginPlay()
+{
+    Super::BeginPlay();
+}
