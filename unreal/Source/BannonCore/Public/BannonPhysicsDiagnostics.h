5b9c51c148306c1e7d307e55990861fc16fca9f0
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 02:56:18 -0400

    Sync local changes for unreal/Source/BannonCore/Public/BannonPhysicsDiagnostics.h

--- unreal/Source/BannonCore/Public/BannonPhysicsDiagnostics.h
+++ unreal/Source/BannonCore/Public/BannonPhysicsDiagnostics.h
@@ -12,4 +12,11 @@ class BANNONCORE_API UBannonPhysicsDiagnostics : public UObject
 public:
     UFUNCTION(BlueprintCallable, Category="Bannon|Diagnostics")
     void CalculateDebugMetrics(float Mass, const FVector& Velocity, const FVector& AngularVelocity, UPARAM(ref) float& OutKineticEnergy, UPARAM(ref) float& OutTotalTorque);
+
+	// Intercepts massive hit vectors and applies a catastrophe-scaled launch multiplier on active ragdolls
+	UFUNCTION(BlueprintCallable, Category="Bannon|Diagnostics")
+	FVector InterceptAndAmplifyForce(class ACharacter* Character, FVector IncomingVelocity, float LaunchMultiplier, float CatastrophicThreshold);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|Diagnostics")
+	float LastCalculatedKineticEnergy;
 };
