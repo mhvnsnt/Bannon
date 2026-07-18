9fb0acecff3a4e866c5fd5478fbc311350facde3
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 14:22:44 -0400

    Implement BannonMassDrivenCollisionHulls

--- unreal/Source/BannonCore/Public/BannonMassDrivenCollisionHulls.h
+++ unreal/Source/BannonCore/Public/BannonMassDrivenCollisionHulls.h
@@ -1,18 +1,15 @@
 #pragma once
-
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "BannonMassDrivenCollisionHulls.generated.h"
 
-// Phase 1 #3: Mass-Driven Collision Hulls
 UCLASS(ClassGroup=(Bannon), meta=(BlueprintSpawnableComponent))
 class BANNONCORE_API UBannonMassDrivenCollisionHulls : public UActorComponent
 {
     GENERATED_BODY()
-
 public:    
     UBannonMassDrivenCollisionHulls();
 
-protected:
-    virtual void BeginPlay() override;
+    UFUNCTION(BlueprintCallable, Category="Bannon|Physics")
+    float CalculateStrikeImpactForce(float AttackerMass, float DefenderMass, FVector LimbVelocity);
 };
