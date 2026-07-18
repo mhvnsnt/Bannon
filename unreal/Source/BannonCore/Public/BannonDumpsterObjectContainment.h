4c938f402edfce8b48a6ca4c81076a4e4ea40eb3
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 14:22:51 -0400

    Implement Dumpster Object Containment physics

--- unreal/Source/BannonCore/Public/BannonDumpsterObjectContainment.h
+++ unreal/Source/BannonCore/Public/BannonDumpsterObjectContainment.h
@@ -1,18 +1,15 @@
 #pragma once
-
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "BannonDumpsterObjectContainment.generated.h"
 
-// Phase 7 #60: Dumpster & Object Containment
 UCLASS(ClassGroup=(Bannon), meta=(BlueprintSpawnableComponent))
 class BANNONCORE_API UBannonDumpsterObjectContainment : public UActorComponent
 {
     GENERATED_BODY()
-
 public:    
     UBannonDumpsterObjectContainment();
 
-protected:
-    virtual void BeginPlay() override;
+    UFUNCTION(BlueprintCallable, Category="Bannon|Sandbox")
+    void CalculateContainmentPhysics(AActor* Dumpster, AActor* ThrownFighter);
 };
