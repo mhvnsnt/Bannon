06aacb05650206f2d7b5d7c3dcd8bdbf348a10a7
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 01:40:29 -0400

    Phase 60: MDickie Models & Crosswalk Systems - BannonMDickieMapping.cpp

---
+++ unreal/Source/BannonCore/Private/BannonMDickieMapping.cpp
@@ -0,0 +1,39 @@
+#include "BannonMDickieMapping.h"
+
+void UBannonMDickieMapping::InitializeMDickieCrosswalk(TArray<FMDickieCrosswalk>& MappingTable)
+{
+    MappingTable.Empty();
+
+    // The Degenerates (D-Generation Sex / DX Parody)
+    MappingTable.Add({"Hall Nighter", "Mike Bail", "/Game/MDickie/Models/MikeBail.b3d"});
+    MappingTable.Add({"Triple X", "Vanderbilt", "/Game/MDickie/Models/Vanderbilt.b3d"});
+    MappingTable.Add({"The Dogg", "Drooner", "/Game/MDickie/Models/Drooner.b3d"});
+    MappingTable.Add({"Ass-Man Billy", "Gunner", "/Game/MDickie/Models/Gunner.b3d"});
+    MappingTable.Add({"X-Kid", "X-Terminator", "/Game/MDickie/Models/XTerminator.b3d"});
+    MappingTable.Add({"Melissa Kennedy", "Stephanie", "/Game/MDickie/Models/Stephanie.b3d"});
+
+    // The Coven of Gnarly (The Brood / Hardy Boyz / E&C Parody)
+    MappingTable.Add({"Grave", "Goth", "/Game/MDickie/Models/Goth.b3d"});
+    MappingTable.Add({"Jett Gnarly", "Jester", "/Game/MDickie/Models/Jester.b3d"});
+    MappingTable.Add({"Mokk Gnarly", "Matador", "/Game/MDickie/Models/Matador.b3d"});
+    MappingTable.Add({"Razor", "Sharp", "/Game/MDickie/Models/Sharp.b3d"});
+    MappingTable.Add({"Crux", "Cross", "/Game/MDickie/Models/Cross.b3d"});
+    MappingTable.Add({"Luna", "Lola", "/Game/MDickie/Models/Lola.b3d"});
+
+    // NWC (New World Covenant / nWo Parody)
+    MappingTable.Add({"The Vandal", "Thunder Muscle", "/Game/MDickie/Models/ThunderMuscle.b3d"});
+    MappingTable.Add({"Vato", "Bloke", "/Game/MDickie/Models/Bloke.b3d"});
+    MappingTable.Add({"Big Cash", "Driver", "/Game/MDickie/Models/Driver.b3d"});
+    MappingTable.Add({"Aaron Reiner", "Mutant", "/Game/MDickie/Models/Mutant.b3d"});
+    MappingTable.Add({"Toxin", "Stardust", "/Game/MDickie/Models/Stardust.b3d"});
+    MappingTable.Add({"Judas Messiah", "Jimi Sierra", "/Game/MDickie/Models/JimiSierra.b3d"});
+
+    // Other Key Main Eventers
+    MappingTable.Add({"John Ford", "Block Buster", "/Game/MDickie/Models/BlockBuster.b3d"});
+    MappingTable.Add({"Cain Elias", "Midwinter", "/Game/MDickie/Models/Midwinter.b3d"});
+    MappingTable.Add({"Vain Abel", "Siberia", "/Game/MDickie/Models/Siberia.b3d"});
+    MappingTable.Add({"The Saint", "Slam Dunk", "/Game/MDickie/Models/SlamDunk.b3d"});
+    MappingTable.Add({"Drake Vane", "Rattlesnake", "/Game/MDickie/Models/Rattlesnake.b3d"});
+    MappingTable.Add({"Hawk", "Jansen", "/Game/MDickie/Models/Jansen.b3d"});
+    MappingTable.Add({"Maroon", "Ackerman", "/Game/MDickie/Models/Ackerman.b3d"});
+}
