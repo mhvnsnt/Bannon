6aae171563a3c9cb056faaa46569ec73ba81a1fd
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 02:56:42 -0400

    Sync local changes for unreal/Source/BannonCore/Public/BannonFBIKComponent.h

--- unreal/Source/BannonCore/Public/BannonFBIKComponent.h
+++ unreal/Source/BannonCore/Public/BannonFBIKComponent.h
@@ -6,6 +6,21 @@
 #include "Components/ActorComponent.h"
 #include "BannonFBIKComponent.generated.h"
 
+USTRUCT(BlueprintType)
+struct FBannonIKTargetData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|IK")
+	FVector TargetLocation = FVector::ZeroVector;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|IK")
+	float ReachAlpha = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|IK")
+	FName JointSocketName = NAME_None;
+};
+
 UCLASS(ClassGroup=(Bannon), meta=(BlueprintSpawnableComponent))
 class BANNONCORE_API UBannonFBIKComponent : public UActorComponent
 {
@@ -19,4 +34,27 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category="Bannon|IK")
 	void UpdateGrappleHandPlacement(FVector TargetBonePosition);
+
+	// Maps full-body IK structures to support procedural hand-placement for rope grapples and turnbuckles
+	UFUNCTION(BlueprintCallable, Category="Bannon|IK")
+	void SnapHandToRope(FVector RopeLocation, bool bIsLeftHand, float EaseDuration);
+
+	UFUNCTION(BlueprintCallable, Category="Bannon|IK")
+	void SnapHandToTurnbuckle(FVector TurnbuckleLocation, bool bIsLeftHand, float EaseDuration);
+
+	// Adjusts skeletal structures for grappling opponents of significantly varying physical proportions
+	UFUNCTION(BlueprintCallable, Category="Bannon|IK")
+	void AdjustGrappleForProportions(float OpponentHeight, float OpponentWeight, const FName& TargetBoneName);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|IK")
+	FBannonIKTargetData LeftHandTarget;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|IK")
+	FBannonIKTargetData RightHandTarget;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|IK")
+	float ProportionAdjustmentScale;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|IK")
+	bool bIsIKActive;
 };
