e769b4a308dd901425fea5294776a0c2a5439dad
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 02:56:29 -0400

    Sync local changes for unreal/Source/BannonCore/Public/BannonJointLockConstraint.h

--- unreal/Source/BannonCore/Public/BannonJointLockConstraint.h
+++ unreal/Source/BannonCore/Public/BannonJointLockConstraint.h
@@ -6,6 +6,24 @@
 #include "Components/ActorComponent.h"
 #include "BannonJointLockConstraint.generated.h"
 
+USTRUCT(BlueprintType)
+struct FBannonJointStressData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|Submission")
+	float CurrentStress = 0.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|Submission")
+	float StressLimit = 100.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|Submission")
+	bool bIsTorn = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|Submission")
+	bool bIsDislocated = false;
+};
+
 UCLASS(ClassGroup=(Bannon), meta=(BlueprintSpawnableComponent))
 class BANNONCORE_API UBannonJointLockConstraint : public UActorComponent
 {
@@ -21,4 +39,18 @@ public:
 	// Submission Leverage Scaling based on height/mass delta
 	UFUNCTION(BlueprintCallable, Category="Bannon|Submission")
 	float CalculateLeverageMultiplier(class ACharacter* Attacker, class ACharacter* Defender);
+
+	// Tracks stress and handles dynamic joint constraint tearing (joint hyper-extension)
+	UFUNCTION(BlueprintCallable, Category="Bannon|Submission")
+	void UpdateTearingStress(FName TargetBone, float TorqueApplied, float DeltaTime);
+
+	// Inflicts joint dislocation/tearing penalty
+	UFUNCTION(BlueprintCallable, Category="Bannon|Submission")
+	void TriggerJointTear(FName TargetBone);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|Submission")
+	TMap<FName, FBannonJointStressData> JointStressMap;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|Submission")
+	float TearThresholdTorque;
 };
