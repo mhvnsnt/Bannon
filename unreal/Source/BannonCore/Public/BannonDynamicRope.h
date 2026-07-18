7b9851f84d57bea4e0588dce2a3f22d46a41b34c
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 02:56:17 -0400

    Sync local changes for unreal/Source/BannonCore/Public/BannonDynamicRope.h

--- unreal/Source/BannonCore/Public/BannonDynamicRope.h
+++ unreal/Source/BannonCore/Public/BannonDynamicRope.h
@@ -8,6 +8,21 @@
 
 class UCableComponent;
 
+USTRUCT(BlueprintType)
+struct FBannonVerletPoint
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|Physics")
+	FVector CurrentPosition = FVector::ZeroVector;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|Physics")
+	FVector OldPosition = FVector::ZeroVector;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|Physics")
+	bool bIsPinned = false;
+};
+
 UCLASS(ClassGroup=(Bannon), meta=(BlueprintSpawnableComponent))
 class BANNONCORE_API UBannonDynamicRope : public UActorComponent
 {
@@ -23,6 +38,29 @@ public:
 	// Multiplier for when a wrestler bounces off the ropes
 	UFUNCTION(BlueprintCallable, Category="Bannon|Physics")
 	FVector CalculateReboundVelocity(FVector IncomingVelocity, float RopeTensionScalar);
+
+	// Simulate Verlet integration for the rope points
+	UFUNCTION(BlueprintCallable, Category="Bannon|Physics")
+	void SimulateVerletRope(float DeltaTime);
+
+	// Solve rope segment constraints to maintain resting distance
+	UFUNCTION(BlueprintCallable, Category="Bannon|Physics")
+	void ResolveVerletConstraints();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|Physics")
+	TArray<FBannonVerletPoint> RopeNodes;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|Physics")
+	float RopeElasticity;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|Physics")
+	float SegmentLength;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|Physics")
+	int32 ConstraintIterations;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|Physics")
+	FVector WindForce;
 
 protected:
 	virtual void BeginPlay() override;
