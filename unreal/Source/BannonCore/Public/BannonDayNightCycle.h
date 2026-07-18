2f42ddf6a5ec725e48ecb2e21b45a656c359a890
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 01:15:31 -0400

    Phase 49-50: Deep AI Tactics & Open World Day/Night - unreal/Source/BannonCore/Public/BannonDayNightCycle.h

--- unreal/Source/BannonCore/Public/BannonDayNightCycle.h
+++ unreal/Source/BannonCore/Public/BannonDayNightCycle.h
@@ -10,9 +10,6 @@ class BANNONCORE_API UBannonDayNightCycle : public UObject
     GENERATED_BODY()
 
 public:
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|Environment")
-    float TimeOfDay; // 0.0 to 24.0
-
     UFUNCTION(BlueprintCallable, Category="Bannon|Environment")
-    void UpdateSunPosition(float DeltaTime, float TimeMultiplier, UPARAM(ref) float& OutSunPitch);
+    void CalculateTimeOfDayLighting(float InGameTimeHours, UPARAM(ref) float& OutDirectionalLightIntensity, UPARAM(ref) float& OutSunAnglePitch);
 };
