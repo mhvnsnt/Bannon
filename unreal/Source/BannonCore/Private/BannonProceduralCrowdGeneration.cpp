1af9717739a01dc85290fa055d4a897dfda0d6fa
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 13:26:30 -0400

    Implement Procedural Crowd Generation stub

---
+++ unreal/Source/BannonCore/Private/BannonProceduralCrowdGeneration.cpp
@@ -0,0 +1,11 @@
+#include "BannonProceduralCrowdGeneration.h"
+
+UBannonProceduralCrowdGeneration::UBannonProceduralCrowdGeneration()
+{
+    PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UBannonProceduralCrowdGeneration::BeginPlay()
+{
+    Super::BeginPlay();
+}
