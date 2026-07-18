e2be657035a0ad3d240668637629699162986329
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 14:22:48 -0400

    Implement Subway Train Interactivity physics

--- unreal/Source/BannonCore/Public/BannonSubwayTrainInteractivity.h
+++ unreal/Source/BannonCore/Public/BannonSubwayTrainInteractivity.h
@@ -1,18 +1,15 @@
 #pragma once
-
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "BannonSubwayTrainInteractivity.generated.h"
 
-// Phase 2 #18: Subway/Train Interactivity
 UCLASS(ClassGroup=(Bannon), meta=(BlueprintSpawnableComponent))
 class BANNONCORE_API UBannonSubwayTrainInteractivity : public UActorComponent
 {
     GENERATED_BODY()
-
 public:    
     UBannonSubwayTrainInteractivity();
 
-protected:
-    virtual void BeginPlay() override;
+    UFUNCTION(BlueprintCallable, Category="Bannon|Sandbox")
+    void ApplyInertialPhysicsToFighters(TArray<AActor*> Occupants, FVector TrainVelocity);
 };
