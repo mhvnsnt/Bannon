64075969028d7dd602df02f53c14737a1e99b6b8
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 14:22:45 -0400

    Implement BannonMassDrivenCollisionHulls

--- unreal/Source/BannonCore/Private/BannonMassDrivenCollisionHulls.cpp
+++ unreal/Source/BannonCore/Private/BannonMassDrivenCollisionHulls.cpp
@@ -5,7 +5,10 @@ UBannonMassDrivenCollisionHulls::UBannonMassDrivenCollisionHulls()
     PrimaryComponentTick.bCanEverTick = false;
 }
 
-void UBannonMassDrivenCollisionHulls::BeginPlay()
+float UBannonMassDrivenCollisionHulls::CalculateStrikeImpactForce(float AttackerMass, float DefenderMass, FVector LimbVelocity)
 {
-    Super::BeginPlay();
+    float MassRatio = AttackerMass / DefenderMass;
+    float Force = MassRatio * LimbVelocity.Size();
+    UE_LOG(LogTemp, Warning, TEXT("Bannon Physics: Mass-driven collision hull strike force: %f"), Force);
+    return Force;
 }
