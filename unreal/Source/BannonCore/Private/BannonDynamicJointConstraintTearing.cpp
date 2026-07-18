2d2713f49659af5b03bce90c8b2240ef7382cee2
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 13:23:39 -0400

    Implement Dynamic Joint Constraint Tearing stub

---
+++ unreal/Source/BannonCore/Private/BannonDynamicJointConstraintTearing.cpp
@@ -0,0 +1,11 @@
+#include "BannonDynamicJointConstraintTearing.h"
+
+UBannonDynamicJointConstraintTearing::UBannonDynamicJointConstraintTearing()
+{
+    PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UBannonDynamicJointConstraintTearing::BeginPlay()
+{
+    Super::BeginPlay();
+}
