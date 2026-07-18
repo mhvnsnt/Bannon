b5e5e3831879128a3da7098327c66d0a83f05e15
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 14:20:02 -0400

    Inject Physics code into ProceduralBalanceRecoveryMatrix stub

--- unreal/Source/BannonCore/Public/BannonProceduralBalanceRecoveryMatrix.h
+++ unreal/Source/BannonCore/Public/BannonProceduralBalanceRecoveryMatrix.h
@@ -4,7 +4,6 @@
 #include "Components/ActorComponent.h"
 #include "BannonProceduralBalanceRecoveryMatrix.generated.h"
 
-// Phase 1 #1: Procedural Balance Recovery Matrix
 UCLASS(ClassGroup=(Bannon), meta=(BlueprintSpawnableComponent))
 class BANNONCORE_API UBannonProceduralBalanceRecoveryMatrix : public UActorComponent
 {
@@ -13,6 +12,20 @@ class BANNONCORE_API UBannonProceduralBalanceRecoveryMatrix : public UActorCompo
 public:    
     UBannonProceduralBalanceRecoveryMatrix();
 
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+    UFUNCTION(BlueprintCallable, Category="Bannon|Physics")
+    void CalculateBalanceState(FVector CenterOfMass, FVector BaseOfSupport, float AngularVelocity);
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|Physics")
+    float RecoveryThreshold;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Bannon|Physics")
+    float FallAngularVelocityLimit;
+
 protected:
     virtual void BeginPlay() override;
+
+private:
+    bool bIsRagdolling;
 };
