e4964091f305ce6e901ef29ba541d5a771c11c69
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 02:56:54 -0400

    Sync local changes for unreal/Source/BannonCore/Public/BannonModelImporter.h

--- unreal/Source/BannonCore/Public/BannonModelImporter.h
+++ unreal/Source/BannonCore/Public/BannonModelImporter.h
@@ -12,4 +12,12 @@ class BANNONCORE_API UBannonModelImporter : public UObject
 public:
     UFUNCTION(BlueprintCallable, Category="Bannon|MDickie")
     void DecryptAndPortLegacyModel(const FString& LegacyFormatPath, UPARAM(ref) FString& OutUnrealAssetPath);
+
+	// Wires decrypted low-poly model meshes directly to active Character skeletal blueprints
+	UFUNCTION(BlueprintCallable, Category="Bannon|MDickie")
+	bool WireMeshToCharacterBlueprint(class ACharacter* Character, const FString& DecryptedMeshPath);
+
+	// Upgrades low-poly legacy texture mappings to Lumen-compatible subsurface scattering dynamic materials
+	UFUNCTION(BlueprintCallable, Category="Bannon|MDickie")
+	bool ApplyLumenMaterialToMesh(class USkeletalMeshComponent* SkeletalMesh, const FString& TexturePath);
 };
