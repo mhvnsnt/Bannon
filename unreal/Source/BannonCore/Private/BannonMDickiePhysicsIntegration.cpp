a6bb4887fcd673980e11423d01225318d8c78ea4
mhvnsnt <fmmg.mars@gmail.com>
2026-07-18 14:31:30 -0400

    Update Chaos Physics Vectors for MDickie moves

--- unreal/Source/BannonCore/Private/BannonMDickiePhysicsIntegration.cpp
+++ unreal/Source/BannonCore/Private/BannonMDickiePhysicsIntegration.cpp
@@ -1,6 +1,7 @@
 #include "BannonMDickiePhysicsIntegration.h"
 #include "BannonMDickieMoves.h"
 #include "GameFramework/Actor.h"
+#include "Components/PrimitiveComponent.h"
 
 UBannonMDickiePhysicsIntegration::UBannonMDickiePhysicsIntegration()
 {
@@ -11,7 +12,31 @@ void UBannonMDickiePhysicsIntegration::BindMDickieMoveToChaos(FString MDickieMov
 {
     if (UBannonMDickieMoves::ValidateMoveExistence(MDickieMoveName))
     {
-        UE_LOG(LogTemp, Warning, TEXT("Bannon MDickie Integration: Binding move %s to Chaos physics outputs for active ragdoll interpolation."), *MDickieMoveName);
+        UPrimitiveComponent* PhysicsComp = Cast<UPrimitiveComponent>(TargetActor->GetRootComponent());
+        if (PhysicsComp && PhysicsComp->IsSimulatingPhysics())
+        {
+            FVector PhysicsImpulse = FVector::ZeroVector;
+            FVector TorqueImpulse = FVector::ZeroVector;
+            
+            if (MDickieMoveName == TEXT("force out of ring"))
+            {
+                PhysicsImpulse = FVector(1000.f, 0.f, 500.f);
+                TorqueImpulse = FVector(0.f, 200.f, 0.f);
+            }
+            else if (MDickieMoveName == TEXT("irish whip"))
+            {
+                PhysicsImpulse = FVector(1500.f, 0.f, 0.f);
+            }
+            else if (MDickieMoveName == TEXT("grappling"))
+            {
+                PhysicsImpulse = FVector(0.f, 0.f, -100.f);
+            }
+            
+            PhysicsComp->AddImpulse(PhysicsImpulse, NAME_None, true);
+            PhysicsComp->AddTorqueInRadians(TorqueImpulse, NAME_None, true);
+            
+            UE_LOG(LogTemp, Warning, TEXT("Bannon MDickie Integration: Bound move %s to Chaos physics outputs with FVector(%f, %f, %f)."), *MDickieMoveName, PhysicsImpulse.X, PhysicsImpulse.Y, PhysicsImpulse.Z);
+        }
     }
     else
     {
