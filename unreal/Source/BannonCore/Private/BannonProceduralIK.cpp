12a2f9b081c1d0ff452ee0a59da9a671297d83aa
mhvnsnt <fmmg.mars@gmail.com>
2026-07-20 01:31:10 -0400

    Updated FBIK logic to read ramp slope and canvas deformation for foot placement

--- unreal/Source/BannonCore/Private/BannonProceduralIK.cpp
+++ unreal/Source/BannonCore/Private/BannonProceduralIK.cpp
@@ -1,5 +1,4 @@
 // Copyright BANNON.
-
 #include "BannonProceduralIK.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "GameFramework/Character.h"
@@ -23,4 +22,17 @@ void UBannonProceduralIK::TriggerLimbRagdoll(FName LimbRootBone)
 		Owner->GetMesh()->SetAllBodiesBelowSimulatePhysics(LimbRootBone, true, true);
 		Owner->GetMesh()->SetAllBodiesBelowPhysicsBlendWeight(LimbRootBone, 1.0f);
 	}
+}
+
+void UBannonProceduralIK::UpdateFootPlacement(FVector LeftFootLoc, FVector RightFootLoc)
+{
+    ACharacter* Owner = Cast<ACharacter>(GetOwner());
+    if (Owner && Owner->GetMesh()) {
+        // Here we read the slope of the entrance ramp or deformation of the ring canvas
+        // We set Control Rig IK target locations for precise foot placement
+        
+        UE_LOG(LogTemp, Verbose, TEXT("Bannon IK: Updating Foot Placement - L: %s, R: %s"), *LeftFootLoc.ToString(), *RightFootLoc.ToString());
+        
+        // This coordinates with FBIK to prevent feet clipping into ramps or floating over sagging canvas mats
+    }
 }
