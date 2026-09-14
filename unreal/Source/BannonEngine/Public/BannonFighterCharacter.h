db41aa1a0eed8138412dde4db07de5115b1c50e5
mhvnsnt <fmmg.mars@gmail.com>
2026-09-14 14:48:08 -0500

    gameplay: attach pose authority to fighter

--- unreal/Source/BannonEngine/Public/BannonFighterCharacter.h
+++ unreal/Source/BannonEngine/Public/BannonFighterCharacter.h
@@ -3,19 +3,23 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "BannonTauntManager.h"
+#include "BannonPoseAuthorityComponent.h"
 #include "BannonFighterCharacter.generated.h"
 
 UCLASS()
 class BANNONENGINE_API ABannonFighterCharacter : public ACharacter
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 public:
     ABannonFighterCharacter();
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Bannon|Pose")
+    TObjectPtr<UBannonPoseAuthorityComponent> PoseAuthority;
+
     UPROPERTY() UBannonTauntManager* TauntManager;
-    
+
     UPROPERTY(BlueprintReadWrite, Category="Bannon|Visual") float HeadCutAlpha;
     UPROPERTY(BlueprintReadWrite, Category="Bannon|Visual") float TorsoBruiseAlpha;
     UFUNCTION(BlueprintCallable, Category="Bannon|Visual") void RefreshDamageMaterials();
-    
     UFUNCTION(BlueprintCallable, Category="Bannon|Taunt") void TriggerTaunt(FName TauntID);
 };
