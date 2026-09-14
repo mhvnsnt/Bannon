c916e96de281a4d9f3f092a37813ff1faf8a1900
mhvnsnt <fmmg.mars@gmail.com>
2026-09-14 17:29:44 -0500

    build: align editor target with UE 5.5

--- unreal/Source/BannonEditor.Target.cs
+++ unreal/Source/BannonEditor.Target.cs
@@ -8,7 +8,7 @@ public class BannonEditorTarget : TargetRules
     {
         Type = TargetType.Editor;
         DefaultBuildSettings = BuildSettingsVersion.V7;
-        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
         ExtraModuleNames.Add("BannonCore");
         ExtraModuleNames.Add("BannonEngine");
     }
